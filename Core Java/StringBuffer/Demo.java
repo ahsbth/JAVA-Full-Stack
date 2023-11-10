7d85b3f2ae50715cd1e1953e0c3f57e2c6ac7a7e
ahsbth <asraj48342@gmail.com>
2023-11-11 00:17:40 +0530

    stringbuffer

---
+++ Core Java/StringBuffer/Demo.java
@@ -0,0 +1,12 @@
+package Core Java.StringBuffer;
+
+public class Demo {
+    public static void main(String[] args){
+        
+       StringBuffer sb=new StringBuffer("hello");
+       System.out.println(sb);
+    
+
+
+    }
+}
